263b236df3b01d6a8b49196daed590a1f727cfb3
cvkenneally <ckbaseball16@gmail.com>
2024-01-04 14:32:01 -0500

    first init

---
+++ src/utils.py
@@ -0,0 +1,7 @@
+
+
+def add_numbers(a, b):
+    return a + b
+
+def is_palindrome(s):
+    return s == s[::-1]
---
+++ test/test_utils.py
@@ -0,0 +1,13 @@
+import pytest
+from src.utils import add_numbers, is_palindrome
+
+def test_add_numbers():
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 1) == 0
+    assert add_numbers(0, 0) == 0
+
+def test_is_palindrome():
+    assert is_palindrome("radar") == True
+    assert is_palindrome("hello") == False
+    assert is_palindrome("A") == True
+    assert is_palindrome("") == True
