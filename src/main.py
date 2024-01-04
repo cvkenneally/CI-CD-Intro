2aebf3e21f5142ef048da27f111f4f218d2a0fbc
cvkenneally <ckbaseball16@gmail.com>
2024-01-04 14:34:30 -0500

    main.py runs functions now

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
-
+from utils import *
 
 def main():
-    print("Hello World!")
+    print(add_numbers(2, 3))
+    print(is_palindrome("radar"))
+    print(is_palindrome("hello"))
+    print(is_palindrome("A"))
+    print(is_palindrome(""))
     
 if __name__ == "__main__":
     main()
